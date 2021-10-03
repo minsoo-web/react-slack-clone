52b6e528930651e7cdeca378b5c71076257ff59d
minsoo kim <zlemzlem5656@naver.com>
2021-10-04 02:58:51 +0900

    [수정] firebase gitignore

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyC3Y3vNb0yT3Gc0bRLPLMIUTncFMAgj2B4",
-  authDomain: "react-slack-clone-e0658.firebaseapp.com",
-  projectId: "react-slack-clone-e0658",
-  storageBucket: "react-slack-clone-e0658.appspot.com",
-  messagingSenderId: "74995718999",
-  appId: "1:74995718999:web:50fd25e187d4197e813c12"
-};
